d6c4703979ffccc03e6484a5371b4aa68950da0c
Lucius Hebert <luciushebert13@gmail.com>
2024-05-02 13:44:43 -0300

    Novas funcionalidades de login e logout

--- usuarios/views.py
+++ usuarios/views.py
@@ -2,12 +2,44 @@ from django.shortcuts import render, redirect
 from usuarios.forms import LoginForms, CadastroForms
 from django.contrib.auth.models import User
 
+#biblioteca para validação de login (bom que só isso aqui)
+from django.contrib import auth
+from django.contrib import messages
 
 def login(request):
     form = LoginForms()
+    
+    #criando login
+    if request.method == 'POST': #POST == Create no CRUD
+        form = LoginForms(request.POST)
+        
+        
+        #Verificar se o formulário é válido
+        if form.is_valid():
+            nome = form['nome_login'].value()
+            senha = form['senha'].value()
+            
+        #"Função" para verificiar se o usuário existe no db
+        usuario = auth.authenticate(
+            request,
+            username=nome,
+            password=senha,
+        )
+        #Se o usuário existir, ele é redirecionado para o index.html (TRUE)
+        if usuario is not None:
+            auth.login(request, usuario)
+            messages.success(request, f'{nome} logado com sucesso!')
+            return redirect('index')
+        #Se não, ele terá que fazer o login novamente
+        else:
+            messages.error(request, 'Erro ao efetuar login.')
+            return redirect('login')
+    
+    
     return render(request, "usuarios/login.html", {'form': form})
 
 def cadastro(request):
+    
     form = CadastroForms()
     
     if request.method == 'POST':
@@ -19,6 +51,7 @@ def cadastro(request):
             
             #Verifica se as senhas são iguais 
             if form["senha_1"].value() != form["senha_2"].value():
+                messages.error(request, 'As senhas não são iguais.')
                 return redirect('cadastro') 
             
             #Se forem iguais, as novas variáveis recebem os dados
@@ -28,6 +61,7 @@ def cadastro(request):
             
             #Verifica se o nome já existe no db
             if User.objects.filter(username=nome).exists():
+                messages.error(request, "O usuário já existe.")
                 return redirect('cadastro') 
             
             
@@ -39,9 +73,12 @@ def cadastro(request):
             )
             #Salva o novo usuário no db
             usuario.save()
+            messages.success(request, f'{nome} cadastrado com sucesso!')
             return redirect('login')
-                
-            
-    
+
     return render(request, "usuarios/cadastro.html", {'form': form})
-    
+
+def logout(request):
+    auth.logout(request)
+    messages.success(request, 'Logout efetuado com sucesso!')
+    return redirect('login')
