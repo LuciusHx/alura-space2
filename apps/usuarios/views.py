8a86aef4604a1676ea2fa0d6183b419b2e9a7a09
Lucius Hebert <luciushebert13@gmail.com>
2024-05-08 12:03:58 -0300

    curso de CRUD

--- apps/usuarios/views.py
+++ apps/usuarios/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from usuarios.forms import LoginForms, CadastroForms
+from apps.usuarios.forms import LoginForms, CadastroForms
 from django.contrib.auth.models import User
 
 #biblioteca para validação de login (bom que só isso aqui)
