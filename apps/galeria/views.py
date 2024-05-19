930d90da5682206446e3e5a6e7da9061b6eff62e
Lucius Hebert <luciushebert13@gmail.com>
2024-05-19 01:04:57 -0300

    Adicionado o CRUD no sistema

--- apps/galeria/views.py
+++ apps/galeria/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from apps.galeria.models import Fotografia
+from apps.galeria.forms import FotografiaForms
 from django.contrib import messages
 
 
@@ -32,10 +33,44 @@ def buscar(request):
     return render(request, "galeria/buscar.html", {"cards": fotografias})
 
 def nova_imagem(request):
-    return render(request, 'galeria/nova_imagem.html')
+    if not request.user.is_authenticated:
+        messages.error(request, 'Usuário não logado')
+        return redirect('login')
+    
+    form = FotografiaForms()
+    if request.method == 'POST':
+        form = FotografiaForms(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Nova fotografia cadastrada!')
+            return redirect(index)
+    return render(request, 'galeria/nova_imagem.html', {'form':form})
 
-def editar_imagem(request):
-    pass
+def editar_imagem(request, foto_id):
+    fotografia = Fotografia.objects.get(id=foto_id)
+    form = FotografiaForms(instance=fotografia)
+    
+    if request.method == 'POST':
+        form = FotografiaForms(request.POST, request.FILES, instance=fotografia)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Fotografia editada com sucesso!')
+            return redirect(index)
 
-def deletar_imagem(request):
-    pass
+    return render(request, 'galeria/editar_imagem.html', {'form':form, 'foto_id':foto_id})
+
+
+def deletar_imagem(request, foto_id):
+    fotografia = Fotografia.objects.get(id=foto_id)
+    fotografia.delete()
+    messages.success(request, 'Apagado com sucesso!')
+    return redirect('index')
+
+def filtro(request, categoria):
+    if categoria == "TODOS":
+        fotografias = Fotografia.objects.order_by("data_fotografia").filter(publicada=True)
+        return render(request, "galeria/index.html", {"cards": fotografias})
+    
+    
+    fotografias = Fotografia.objects.order_by("data_fotografia").filter(publicada=True, categoria=categoria)
+    return render(request, 'galeria/index.html', {'cards':fotografias})
